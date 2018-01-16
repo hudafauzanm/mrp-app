4b592603df8635d840f6b2f17de10320d9d7e0d4
Akbar Ahmadi <hero.lionel@gmail.com>
2018-01-16 11:38:08 +0700

    fix lagi

--- app/Pegawai.php
+++ app/Pegawai.php
@@ -7,24 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 class Pegawai extends Model
 {
     protected $table = 'pegawai';
+    protected $primaryKey='id';
+    public $incrementing = false;
+
+    protected $hidden = [
+        'id', 'password', 'user_role'
+    ];
+    
 
     public function formasi_jabatan()
     {
-    	return $this->belongsTo('App\FormasiJabatan')
+    	return $this->belongsTo('App\FormasiJabatan');
     }
 
     public function diklat_penjenjangan()
     {
-    	return $this->hasMany('App\DiklatPenjenjangan')
+    	return $this->hasMany('App\DiklatPenjenjangan');
     }
 
     public function mrp()
     {
-    	return $this->hasMany('App\MRP')
+    	return $this->hasMany('App\MRP');
     }
 
     public function penilaian_pegawai()
     {
         return $this->hasMany('App\PenilaianPegawai');
+    }
+
+    public function time_diff($dari, $ke)
+    {
+        return $dari->diff($ke)->format('%y tahun, %m bulan, %d hari');
     }
 }
