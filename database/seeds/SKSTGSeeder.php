749921c1c9a8d1c82602ab5c8f579bf3a8bfcb42
Hania Maghfira <34994959+seehani@users.noreply.github.com>
2018-01-29 20:51:04 +0700

    edit SKSTGseeder

--- database/seeds/SKSTGSeeder.php
+++ database/seeds/SKSTGSeeder.php
@@ -14,12 +14,12 @@ class SKSTGSeeder extends Seeder
     {
         $skstg = new skstg;
         $skstg->id = Uuid::generate();
-        $skstg->no_dokumen_proses_sk = '';
         $skstg->tahun_sk = '';
         $skstg->no_sk = '';
         $skstg->no_dokumen_kirim_sk = '';
-        $skstg->tanggal_kirim_sk = '';
-        $skstg->tanggal_aktivasi = '';
+        $skstg->filename_dokumen_sk = '';
+        $skstg->tgl_kirim_sk = '';
+        $skstg->tgl_aktivasi = '';
         $skstg->no_stg = '';
         $skstg->mrp_id = $mrp->id;
         $skstg->save();
